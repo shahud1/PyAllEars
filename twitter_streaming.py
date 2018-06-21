b82236955ae527f24a94548b35cf1a69a6f169e0
Punchok Kerdsiri <punch872@gmail.com>
2018-06-21 10:03:24 +0700

    fix and removed unessary code
    -add straming py
    -fix bug

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -1,4 +1,3 @@
-# Import the necessary package to process data in JSON format
 try:
     import json
 except ImportError:
@@ -6,6 +5,7 @@ except ImportError:
 
 # Import the necessary methods from "twitter" library
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
 
 # Variables that contains the user credentials to access Twitter API
 ACCESS_TOKEN = '1008737228166594560-4h4YeV2P0xHXMVyFsGGatnweV7OEQJ'
@@ -16,10 +16,18 @@ CONSUMER_SECRET = 'fLvpGKK3o1mo3onKyMWi6FFMtKeAwT70qKoxhjpylc8FRBHJDo'
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 
 # Initiate the connection to Twitter Streaming API
-twitter_stream = TwitterStream(auth=oauth)
+twitter_userstream = TwitterStream(auth=oauth, domain='userstream.twitter.com')
+twitter_stream = TwitterStream(auth=oauth, domain='stream.twitter.com')
 
 # Get a sample of the public data following through Twitter
-iterator = twitter_stream.statuses.sample()
+#parameter
+#follow	        optional	A comma separated list of user IDs, indicating the users to return statuses for in the stream. See follow for more information.
+#track	        optional	Keywords to track. Phrases of keywords are specified by a comma-separated list. See track for more information.
+#locations	    optional	Specifies a set of bounding boxes to track. See locations for more information.
+#delimited	    optional	Specifies whether messages should be length-delimited. See delimited for more information.
+#stall_warnings	optional	Specifies whether stall warnings should be delivered. See stall_warnings for more information.
+
+iterator = twitter_stream.statuses.filter(follow='3151431577')
 
 # Print each tweet in the stream to the screen
 # Here we set it to stop after getting 1000 tweets.
@@ -31,7 +39,7 @@ for tweet in iterator:
     # Twitter Python Tool wraps the data returned by Twitter
     # as a TwitterDictResponse object.
     # We convert it back to the JSON format to print/score
-    print json.dumps(tweet)
+    print(json.dumps(tweet))
 
     # The command below will do pretty printing for JSON data, try it out
     # print json.dumps(tweet, indent=4)
