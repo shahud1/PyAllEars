610a2455ef2b3989395995873591de9de23985c6
Punchok Kerdsiri <punch872@gmail.com>
2018-06-30 21:54:53 +0700

    LastUpdate
    LastUp

--- twitter_Streaming_API/Tweet_Mining and Deep_analyze/Twitter_stream_api.py
+++ twitter_Streaming_API/Tweet_Mining and Deep_analyze/Twitter_stream_api.py
@@ -4,11 +4,13 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 #Variables that contains the user credentials to access Twitter API
-access_token = "ENTER YOUR ACCESS TOKEN"
-access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
-consumer_key = "ENTER YOUR API KEY"
-consumer_secret = "ENTER YOUR API SECRET"
-
+consumer_key = 'vmYHpTCOo34lqzrWSkdnhyd10'
+# put your consumer_secret from Twitter API
+consumer_secret = '4yR2GHiLQoKh71GDZgVAA8LEIJIuHT2DTMbPoOoV649rHzIpdW'
+# put your access_token from Twitter API
+access_token = '1008737228166594560-aGuXlq6Uqdg6AsDhGKMPm26eNAjeUV'
+# put your access_secret from Twitter API
+access_secret = 'h5Xy0Te0lCxmgXLyOFXGwELF29gJtYtvxg5DjW16cgOFG'
 
 #basic listener to print tweet recieved
 class StdOutListener(StreamListener):
@@ -20,12 +22,12 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_token, access_secret)
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: '#worldcup', 
     # 'Luis Suarez', 'Cristiano Ronaldo',
     stream.filter(track=['#worldcup', 'Luis Suarez',
-                         'Cristiano Ronaldo','Neymar','Lionel Messi'])
+                         'Cristiano Ronaldo','Neymar','Lioçnel Messi'])
 
 
