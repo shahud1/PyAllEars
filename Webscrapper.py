24c2e3a7738a4a6f61cb2996f1e39b9e5cba9766
Your Name <you@example.com>
2018-06-30 22:38:49 +0700

    Cleanup

--- Webscrapper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-# Enter the web address that you want to fetch 
-page = requests.get("http://ict.siit.tu.ac.th/~stanislav/ComputerGraphics/")
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# Part of this code can also be used for DhiNLP to gather data from various documents online.
